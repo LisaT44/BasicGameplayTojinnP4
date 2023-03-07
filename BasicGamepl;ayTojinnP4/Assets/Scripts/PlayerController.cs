4f53047d738d1482c6a85c383e1a4cc0cb5d376c
ElisaTojin <elisactojin@gmail.com>
2023-03-07 11:39:35 -0800

    I worked on my project
    I made the food a prjectile and a prefab.
    I also made the animals  prefabs

--- BasicGamepl;ayTojinnP4/Assets/Scripts/PlayerController.cs
+++ BasicGamepl;ayTojinnP4/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Keep the playter in bounds
+        //Keep the playter in bounds-
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -29,5 +31,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
