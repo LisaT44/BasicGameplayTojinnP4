c96da6f3e7737e5627276beb620f85f4cfa7fcb6
ElisaTojin <elisactojin@gmail.com>
2023-03-09 11:40:31 -0800

    I finsihed the lessons
    i finsaeh thwsa lessona dn started tyher bonus featur5eess

--- BasicGamepl;ayTojinnP4/Assets/Scripts/PlayerController.cs
+++ BasicGamepl;ayTojinnP4/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public float zRange = 10.0f;
 
     public GameObject projectilePrefab;
 
@@ -19,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Keep the playter in bounds-
+        //Keep the playter in bounds
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -29,8 +31,21 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.x < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        if (transform.position.x > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
