65e8fecce5c895a4c596c8c7f6a5e6e9bfbc62ee
ElisaTojin <elisactojin@gmail.com>
2023-03-13 13:05:19 -0700

    Finsihed
    I finsished my project and i recorded the video and turned it in

--- BasicGamepl;ayTojinnP4/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGamepl;ayTojinnP4/Assets/Scripts/DestroyOutOfBounds.cs
@@ -25,15 +25,17 @@ public class DestroyOutOfBounds : MonoBehaviour
         } 
         else if(transform.position.z < lowerBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if(transform.position.x > sideBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if(transform.position.x < -sideBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
     }
